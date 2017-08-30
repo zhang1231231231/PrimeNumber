57a33688ca1a4f8e7a8f0938883b23d86be62fc4
zhang1231231231 <625452708@qq.com>
2017-08-30 22:31:44 +0800

    检测输入的数是否是素数！

--- PrimeNumber/MainWindow.xaml.cs
+++ PrimeNumber/MainWindow.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Numerics;
+using System.Security.Cryptography;
 
 namespace PrimeNumber
 {
@@ -23,6 +25,129 @@ namespace PrimeNumber
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        //检测粘贴
+        private void textBox1_Pasting(object sender, DataObjectPastingEventArgs e)
+        {
+            if (e.DataObject.GetDataPresent(typeof(String)))
+            {
+                String text = (String)e.DataObject.GetData(typeof(String));
+                if (!isNumberic(text))
+                { e.CancelCommand(); }
+            }
+            else { e.CancelCommand(); }
+        }
+
+        private void textBox1_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Space)
+                e.Handled = true;
+        }
+
+        private void textBox1_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            if (!isNumberic(e.Text))
+            {
+                e.Handled = true;
+            }
+            else
+                e.Handled = false;
+        }
+
+        //isDigit是否是数字
+        public static bool isNumberic(string _string)
+        {
+            if (string.IsNullOrEmpty(_string))
+                return false;
+            foreach (char c in _string)
+            {
+                if (!char.IsDigit(c))
+                    //if(c<'0' c="">'9')//最好的方法,在下面测试数据中再加一个0，然后这种方法效率会搞10毫秒左右
+                    return false;
+            }
+            return true;
+        }
+
+        /// <summary>
+        /// 判断输入的数字是素数
+        /// </summary>
+        /// <param name="source">输入的大数</param>
+        /// <returns></returns>
+        public bool IsProbablePrime(BigInteger source)
+        {
+            int certainty = 2;
+            if (source == 2 || source == 3)
+                return true;
+            if (source < 2 || source % 2 == 0)
+                return false;
+
+            BigInteger d = source - 1;
+            int s = 0;
+
+            while (d % 2 == 0)
+            {
+                d /= 2;
+                s += 1;
+            }
+            RandomNumberGenerator rng = RandomNumberGenerator.Create();
+            byte[] bytes = new byte[source.ToByteArray().LongLength];
+            BigInteger a;
+
+            for (int i = 0; i < certainty; i++)
+            {
+                do
+                {
+                    rng.GetBytes(bytes);
+                    a = new BigInteger(bytes);
+                }
+                while (a < 2 || a >= source - 2);
+
+                BigInteger x = BigInteger.ModPow(a, d, source);
+                if (x == 1 || x == source - 1)
+                    continue;
+
+                for (int r = 1; r < s; r++)
+                {
+                    x = BigInteger.ModPow(x, 2, source);
+                    if (x == 1)
+                        return false;
+                    if (x == source - 1)
+                        break;
+                }
+
+                if (x != source - 1)
+                    return false;
+            }
+            return true;
+        }
+
+        private void CheckNumberButton_Click(object sender, RoutedEventArgs e)
+        {
+            if(isNumberic(textBox1.Text))
+            {
+                BigInteger number = new BigInteger();
+                var result = BigInteger.TryParse(textBox1.Text, out number);
+                if(result)
+                {
+                   if(IsProbablePrime(number))
+                    {
+                        ResultText.Text = "恭喜，是素数！";
+                    }
+                    else
+                    {
+                        ResultText.Text = "不是素数！";
+                    }
+                }
+                else
+                {
+                    ResultText.Text = "请在输入框输入合法的数字";
+                }
+            }
+            else
+            {
+                ResultText.Text = "请在输入框输入数字";
+            }
         }
     }
 }
